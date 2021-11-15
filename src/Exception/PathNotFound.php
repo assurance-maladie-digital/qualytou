2579cac950e0b64d601633ed9767577a0c0806cc
Mathias STRASSER <contact@roukmoute.fr>
2021-11-15 10:38:05 +0100

    Monte de version phpstan
    Met à jours phpstan et ses extensions

---
+++ src/Exception/PathNotFound.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PHPStaticAnalysisTool\Exception;
+
+use Exception;
+
+class PathNotFound extends Exception
+{
+    public function __construct(string $path)
+    {
+        parent::__construct(\sprintf('Le chemin pour "%s" n\'a pas été trouvé.', $path));
+    }
+}
