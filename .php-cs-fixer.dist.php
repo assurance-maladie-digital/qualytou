f414e58f9cce0cbc0bdc1957ad17c9baae7b8b0f
bidule <christophe.gaon@assurance-maladie.fr>
2023-10-03 11:38:09 +0200

    upgrade and simplify conf phpcs fixer

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -19,61 +19,18 @@ return (new PhpCsFixer\Config())
         [
             '@DoctrineAnnotation' => true,
             '@Symfony' => true,
-            '@Symfony:risky' => true,
-
-            // @Symfony code styles rules blacklisting:
-            'method_chaining_indentation' => true,
-            'no_singleline_whitespace_before_semicolons' => true,
-            'no_trailing_comma_in_list_call' => false,
-            'php_unit_fqcn_annotation' => false,
-            'phpdoc_align' => false,
-            'phpdoc_annotation_without_dot' => false,
-            'phpdoc_indent' => false,
-            'phpdoc_inline_tag_normalizer' => false,
-            'phpdoc_no_access' => false,
-            'phpdoc_no_alias_tag' => false,
-            'phpdoc_no_empty_return' => false,
-            'phpdoc_no_package' => false,
-            'phpdoc_no_useless_inheritdoc' => false,
-            'phpdoc_return_self_reference' => false,
-            'phpdoc_scalar' => false,
-            'phpdoc_separation' => false,
-            'phpdoc_single_line_var_spacing' => false,
-            'phpdoc_summary' => false,
-            'phpdoc_to_comment' => false,
-            'phpdoc_trim' => false,
-            'phpdoc_types' => false,
-            'phpdoc_var_without_name' => false,
-            'error_suppression' => false,
-            'standardize_not_equals' => false,
-
-            // @Symfony customised rules
-            'concat_space' => ['spacing' => 'one'],
-            'native_function_invocation' => false,
-            'single_quote' => ['strings_containing_single_quote_chars' => true],
-            'visibility_required' => ['elements' => ['property', 'method', 'const']],
+            // Pas de yoda style
             'yoda_style' => ['equal' => false, 'identical' => false, 'less_and_greater' => false],
-
-            // Additional code style rules whitelisting:
-            'align_multiline_comment' => true,
-            'array_indentation' => true,
-            'array_syntax' => ['syntax' => 'short'],
-            'combine_consecutive_issets' => true,
-            'declare_strict_types' => true,
-            'explicit_indirect_variable' => true,
-            'explicit_string_variable' => true,
-            'fully_qualified_strict_types' => true,
-            'global_namespace_import' => ['import_classes' => false, 'import_constants' => false, 'import_functions' => false],
-            'list_syntax' => ['syntax' => 'short'],
-            'mb_str_functions' => true,
-            'multiline_comment_opening_closing' => true,
-            'multiline_whitespace_before_semicolons' => ['strategy' => 'new_line_for_chained_calls'],
-            'no_alternative_syntax' => true,
-            'no_superfluous_elseif' => true,
-            'ordered_interfaces' => true,
+            // Conserver les deux étoiles en début de commentaire - Indispensable pour phpstan et psalm
+            'phpdoc_to_comment' => false,
+            'comment_to_phpdoc' => true,
+            // On met un espace avant et après la concaténation
+            'concat_space' => ['spacing' => 'one'],
+            // Problème d'import : https://github.com/PHP-CS-Fixer/PHP-CS-Fixer/issues/7257
+            'global_namespace_import' => ['import_classes' => true],
         ]
     )
     ->setRiskyAllowed(true)
     ->setFinder($finder)
     ->setCacheFile('.php-cs-fixer.cache')
-;
+    ;
