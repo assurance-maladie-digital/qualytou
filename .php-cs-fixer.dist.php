9e293718bad86ca242c6ddf6ccfdb6742853fdc0
Mathias STRASSER <contact@roukmoute.fr>
2024-02-01 14:54:43 +0100

    Tri des lignes

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -18,17 +18,18 @@ return (new PhpCsFixer\Config())
         [
             '@DoctrineAnnotation' => true,
             '@Symfony' => true,
-            // Pas de yoda style
-            'yoda_style' => ['equal' => false, 'identical' => false, 'less_and_greater' => false],
             // Conserver les deux étoiles en début de commentaire - Indispensable pour phpstan et psalm
-            'phpdoc_to_comment' => false,
             'comment_to_phpdoc' => true,
             // On met un espace avant et après la concaténation
             'concat_space' => ['spacing' => 'one'],
-            // On n'importe pas les classes/fonctions/constantes globales
-            'global_namespace_import' => ['import_classes' => false],
             // On force la déclaration des types stricts
             'declare_strict_types' => true,
+            // On n'importe pas les classes/fonctions/constantes globales
+            'global_namespace_import' => ['import_classes' => false],
+            // Les Docblocks ne doivent être utilisés que sur des éléments structurels
+            'phpdoc_to_comment' => false,
+            // Pas de yoda style
+            'yoda_style' => ['equal' => false, 'identical' => false, 'less_and_greater' => false],
         ]
     )
     ->setRiskyAllowed(true)
